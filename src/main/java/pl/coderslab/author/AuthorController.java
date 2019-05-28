343f2a9d53f0badd2bd43ddd6a558d13fbe587fb
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-05-28 16:25:05 +0200

    Zadania Hibernate dzien 2

---
+++ src/main/java/pl/coderslab/author/AuthorController.java
@@ -0,0 +1,60 @@
+package pl.coderslab.author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/list")
+    public String getList(Model model) {
+        List<Author> authors = authorService.findAll();
+        model.addAttribute("authors", authors);
+        return "authorList";
+    }
+
+    @GetMapping("/add")
+    public String addAuthor(Model model) {
+        model.addAttribute("author", new Author());
+        return "author";
+    }
+
+    @PostMapping("/add")
+    public String addAuthor(@ModelAttribute Author author){
+        authorService.saveAuthor(author);
+        return "redirect:list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        Author author = authorService.findAuthor(id);
+        model.addAttribute("author", author);
+        return "author";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@ModelAttribute Author author) {
+        authorService.updateAuthor(author);
+        return "redirect:../list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return "redirect:../list";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/author.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<form:form method="post" modelAttribute="author">
+    <label for="firstNameId">First name:</label>
+    <form:input type="text" path="firstName" id="firstNameId"/>
+    <br><br>
+
+    <label for="lastNameId">Last name:</label>
+    <form:input type="text" path="lastName" id="lastNameId"/>
+    <br><br>
+
+    <input type="submit" value="Save">
+</form:form>
+</body>
+</html>
