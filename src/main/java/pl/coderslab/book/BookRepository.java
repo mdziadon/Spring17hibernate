c0d941e4df65eb0e11959332db3ab1d1a3aa6a0d
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-05-30 12:49:42 +0200

    Zadania Hibernate dzien 4

--- src/main/java/pl/coderslab/book/BookRepository.java
+++ src/main/java/pl/coderslab/book/BookRepository.java
@@ -1,6 +1,9 @@
 package pl.coderslab.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -8,7 +11,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(String title);
 
+    @Query("select b from Book b where b.title = ?1")
+    List<Book> findByTitleQuery(String title);
+
     List<Book> findByCategoryId(Long id);
+
+    @Query("select b from Book b where b.category.id = :categoryId")
+    List<Book> findByCategoryIdQuery(@Param("categoryId") Long id);
 
     List<Book> findByAuthorsId(Long id);
 
@@ -19,4 +28,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findFirstByCategoryIdOrderByTitle(Long id);
 
     List<Book> findAllByProposition(boolean proposition);
+
+    @Modifying
+    @Query(value = "delete from book_authors where book_id = ?1", nativeQuery = true)
+    void deleteBookRelations(Long id);
+
 }
